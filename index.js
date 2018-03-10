94d3187711a5bfcaf26abc110bec7d34dd5cbd66
Anthony <aengland94@gmail.com>
2018-03-09 18:11:45 -0700

    postal rate calculator

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 var smath = require('./smath')
+var postal = require('./postal')
+var bodyParser = require('body-parser');
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000))
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))  // for accessing static files
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+  response.render('pages/index')
+})
 
 app.get('/math', (req, res) => {
    var params = smath(req, res)
    res.render('pages/mathResults', params)
 })
 
+app.post('/postal', (req, res) => {
+      var params = postal(req.body.type, req.body.weight, res)
+      res.render('pages/postalResults', params)   
+})
+
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+  console.log('Node app is running on port', app.get('port'))
+})
---
+++ postal.js
@@ -0,0 +1,108 @@
+module.exports = function getRate(type, weight, res)
+{
+   var result = calculateRate(type, weight)
+
+   if (result == null)
+   {
+      res.writeHead(404)
+      return res.end()
+   }
+
+   var params = {type: type, 
+                 weight: weight, 
+                 cost: result}
+
+   return params
+}
+
+function calculateRate(type, weight)
+{
+   if (type == 'Letters (Stamped)') 
+      return getStamped(weight)
+   else if (type == 'Letters (Metered)') 
+      return getMetered(weight)
+   else if (type == 'Large Envelopes (Flats)') 
+      return getFlats(weight)
+   else if (type == 'Parcels') 
+      return getParcels(weight)
+}
+
+function getStamped(weight)
+{
+   weight = Number(weight)
+
+   if (weight <= 1) 
+      return 0.50
+   else if (weight <= 2) 
+      return 0.71
+   else if (weight <= 3) 
+      return 0.92
+   else
+      return 1.13
+}
+
+function getMetered(weight)
+{
+   weight = Number(weight)
+
+   if (weight <= 1) 
+      return 0.47
+   else if (weight <= 2) 
+      return 0.68
+   else if (weight <= 3) 
+      return 0.89
+   else
+      return  1.10
+}
+
+function getFlats(weight)
+{
+   weight = Number(weight)
+
+   if (weight <= 1) 
+      return 1.00
+   else if (weight <= 2) 
+      return 1.21
+   else if (weight <= 3) 
+      return 1.42
+   else if (weight <= 4) 
+      return 1.63
+   else if (weight <= 5) 
+      return 1.84
+   else if (weight <= 6) 
+      return 2.05
+   else if (weight <= 7) 
+      return 2.26
+   else if (weight <= 8) 
+      return 2.47
+   else if (weight <= 9) 
+      return 2.68
+   else if (weight <= 10) 
+      return 2.89
+   else if (weight <= 11) 
+      return 3.10
+   else if (weight <= 12) 
+      return 3.31
+   else
+      return 3.52
+}
+
+function getParcels(weight)
+{
+   weight = Number(weight)
+
+   if (weight <= 4) 
+      return 3.50
+   else if (weight <= 8) 
+      return 3.75
+   else if (weight <= 9) 
+      return 4.10
+   else if (weight <= 10) 
+      return 4.45
+   else if (weight <= 11) 
+      return 4.80
+   else if (weight <= 12) 
+      return 5.15
+   else
+      return 5.50
+}
